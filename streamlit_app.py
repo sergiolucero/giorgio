274afb42b76ae12501c7cafaf8e1b108069ff56a
Sergio Lucero <sergiolucero@gmail.com>
2023-09-04 19:31:26 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,14 @@
 import streamlit as st
 from awslib import s3_contents
-from textlib import droplines
 from linelib import simple_recorder 
 ##############################################
 st.set_page_config(layout="wide")
 
 tab1, tab2 = st.tabs(['Grabación','Revisión'])
 with tab1:
-    st.title('👨‍⚕️CETRAM QuantMed LLM Doctor🤖')
-    for dropline in droplines:
-        st.write(dropline)
+    st.title('👩‍🏫 Colegio San Juan escriba digital 🤖')
     simple_recorder()
 with tab2:
-    st.header('Contents of cetram-felix/AUDIO')
+    st.header('Revisión)
     contents = s3_contents()  
     st.table(contents)
